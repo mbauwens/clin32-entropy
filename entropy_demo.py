566fd6f3fe3cf27a6535709319cb58bc1e881f15
mbauwens <michael.bauwens@ucll.be>
2022-06-16 23:31:11 +0000

    finalise notebook & add jupytext paired python file

---
+++ entropy_demo.py
@@ -0,0 +1,113 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.8
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Limitations of the entropy measure in n-gram language modelling (CLIN32)
+
+# ### by Michael Bauwens (UCLL Research & Expertise)
+
+# This demo accompanies the poster presentation "Limitations of the entropy measure in n-gram language modelling". It provides the functionality to:
+# - import data (default: Jane Austen's "Emma")
+# - train a trigram language model on this dataset
+# - compute the probabilities of every sentence in the dataset
+# - score every sentence with a variety of entropy measures
+# - explore the correlations between entropy measures
+# - retrieve the most probable sentences (low entropy) based on every measure
+
+# ## Import required modules
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
+from tools import *
+from nltk import download
+from nltk.corpus import gutenberg
+
+# ## Import the data
+
+# As toy example, we'll use Jane Austen's "Emma" (provided by the Gutenberg project via NLTK).
+
+download('gutenberg')
+data = [[w.lower() for w in s] for s in gutenberg.sents('austen-emma.txt')]
+
+# If you want to try this notebook with your own dataset, you can provide the text files. The function `import_data` _should_ transform the data into the correct format for the language modelling (a list of sentences, each consisting of lists of tokenised words). Currently the function is configured to support Dutch and English via the `lang` parameter. Should you require a different language, you can adapt the function (for language supported in spaCy v3, see [spacy.io/usage/models](https://spacy.io/usage/models)).
+
+# +
+# data = import_data("/path/to/directory/", lang="ens")
+# -
+
+# ## Train the language model
+
+# We'll use a trigram statistical language model, using a vocabulary with cutoff 2. We're using the standard language model and not a model with any smoothing (e.g. Kneser-Ney Interpolated smoothing) because we'll just evaluate the probability of sentences within the corpus *itself*.
+
+lm = train_lm(data, cutoff=2)
+
+# ## Compute probabilities for every sentence
+
+# For every sentence we'll compute the raw probabilities and store them in a dictionary together with the string version of the sentence and a list of the trigrams in the sentence. Additionally, we'll create a frequency distribution of the trigrams in the dataset, which we'll need for the frequency weighting later on. 
+
+fdist, sent_dict = score_sentences(data, lm)
+
+# You can explore the sentence probabilities using the `get_random_sample(sent_dict)` function.
+
+get_random_sample_prob(sent_dict)
+
+# ## Calculate entropy measures
+
+# For each sentence, we add the different entropy measures to the dictionary as well as some additional information (sentence length, percentage of trigrams with low relative frequency, and percentage of trigrams with perfect probability).
+#
+# Entropy measures:
+# - Shannon entropy
+# - Length normalised Shannon entropy
+# - Shannon entropy via Shannon-McMillan-Breiman theorem
+# - Relative frequency weighted Shannon entropy
+# - Length normalised relative frequency weighted Shannon entropy
+#
+# The dictionary is then transformed into a Pandas dataframe.
+
+# +
+for sent_id in sent_dict:
+    sent_dict[sent_id] = entropy_measures(sent_dict[sent_id], fdist, lm)
+    
+df = pd.DataFrame.from_dict(sent_dict, orient='index')
+# -
+
+# Check out some random samples.
+
+get_random_sample_ent(df)
+
+# Now we'll investigate how the different measures relate to one another in terms of correlation.
+
+get_corr_pairplot(df)
+
+# ### Sentences with lowest entropy
+
+# For Shannon entropy, length normalised Shannon entropy, and length normalised relative frequency weighted Shannon entropy we'll display the 10 sentences with lowest entropy.
+
+df.sort_values(by=['shan']).head(10)
+
+df.sort_values(by=['shan_lengthnorm']).head(10)
+
+df.sort_values(by=['shan_length_relfreq']).head(10)
+
+# If you want to look up the frequency specifics of a certain sentence, you can use `get_freq_counts` to get frequency counts for the tri- and unigrams.
+
+get_freq_counts(df, lm, fdist, 420)
+
+# Finally, we'll visualise the difference between Shannon entropy and length normalised relative frequency weighted Shannon entropy in terms of their correlation.
+
+print(stats.pearsonr(df['shan_length_relfreq'], df['shan']))
+plt.scatter(df['shan_length_relfreq'], df['shan'], color='#002757')
+plt.xlabel("Length normalised frequency weighted Shannon entropy")
+plt.ylabel("Shannon entropy")
--- tools.py
+++ tools.py
@@ -5,8 +5,11 @@ from nltk.lm.preprocessing import padded_everygram_pipeline, pad_both_ends
 from numpy import log2, mean, random, array
 import seaborn as sns
 from scipy import stats
+import matplotlib.pyplot as plt
 
 def import_data(datadir, lang):
+    """Import dataset, sentence and word tokenise it (spaCy) and turn it into a list of token lists."""
+    data = []
     if lang == 'nl':
         from spacy.lang.nl import Dutch
         nlp = Dutch()
@@ -27,6 +30,7 @@ def import_data(datadir, lang):
     return data
 
 def train_lm(corpus, cutoff=2):
+    """Create a Maximum Likelihood Estimator language model (nltk) with padded trigrams."""
     train, vocab = padded_everygram_pipeline(3, corpus)
     
     if cutoff > 1:
@@ -40,10 +44,11 @@ def train_lm(corpus, cutoff=2):
     return lm
 
 def get_trigrams(sentence):
-    # the sentence should be formatted as a list of words
+    """Turn a sentence (formatted as a list of tokens) into trigrams."""
     return trigrams(pad_both_ends(sentence, 3))
 
-def populate_dicts(corpus, lm):
+def score_sentences(corpus, lm):
+    """Score sentences with probabilities according to the language model and make a (relative) frequency distribution."""
     fdist = FreqDist()
     sent_dict = {}
     for i, sent in enumerate(corpus):
@@ -68,6 +73,7 @@ def populate_dicts(corpus, lm):
     return fdist, sent_dict
 
 def get_random_sample_prob(sent_dict):
+    """Retrieve a random sentence with its trigrams and probabilities."""
     print("Random sample of a sentence with its probabilities\n")
     sent_amount = len(sent_dict)
     rand_id = random.randint(0,sent_amount+1)
@@ -78,17 +84,20 @@ def get_random_sample_prob(sent_dict):
     )
 
 def shannon_entropy(sent_dict_entry):
-    """Shannon Entropy: negative sum over all probabilities*log2_probabilities
-    https://en.wikipedia.org/wiki/Entropy_(information_theory)
+    """Shannon entropy: negative sum over all probabilities*log2_probabilities
+    Shannon (1948) ; https://en.wikipedia.org/wiki/Entropy_(information_theory)
     See chapter 3 of Speech and Language Processing (Jurafsky and Martin, 2021) Formula 3.41.
     """
     return -1 * sum([prob * log2(prob) for prob in sent_dict_entry['prob_list']])
 
 def length_normalised_shannon_entropy(sent_dict_entry):
-    """Length normalised Shannon Entropy: divide Shannon entropy by the amount of trigrams in the sentence."""
+    """Length normalised Shannon entropy: divide Shannon entropy by the amount of trigrams in the sentence."""
     return shannon_entropy(sent_dict_entry) / len(sent_dict_entry['prob_list'])
 
 def relfreq_weighted_shannon_entropy(sent_dict_entry, fdist, lm, length_normalisation=False):
+    """Relative frequency weighted Shannon entropy: weight each probability used in the Shannon entropy formula by the
+    relative frequency of the trigram. In case of <UNK> tokens, weight with the minimum relative frequency in the dataset.
+    Also includes the possibility to length normalise."""
     sent_dict_entry_cp = sent_dict_entry
     def contains_UNK(tri):
         bool_list = []
@@ -101,6 +110,7 @@ def relfreq_weighted_shannon_entropy(sent_dict_entry, fdist, lm, length_normalis
 
     weighted_prob_list = []
     for prob, tri in zip(sent_dict_entry['prob_list'], sent_dict_entry['trigrams']):
+        tri = ' '.join(tri)
         if contains_UNK(tri):
             prob *= min(fdist['rel_fd'].values())
         else:
@@ -118,7 +128,9 @@ def shannon_mcmillan_breiman_entropy(sent_dict_entry):
     """
     return -1 * mean([log2(prob) for prob in sent_dict_entry['prob_list']])
 
-def entropy_measures(sent_dict_entry, fdist, lm, verbose=False):
+def entropy_measures(sent_dict_entry, fdist, lm):
+    """Compute every entropy measure and sentence length, percentage of low trigram relfreq,
+    and percentage of perfect probability."""
     shan = shannon_entropy(sent_dict_entry)
     shan_lengthnorm = length_normalised_shannon_entropy(sent_dict_entry)
     shan_mcmill_brei = shannon_mcmillan_breiman_entropy(sent_dict_entry)
@@ -128,19 +140,6 @@ def entropy_measures(sent_dict_entry, fdist, lm, verbose=False):
     sent_length = len(sent_dict_entry['prob_list'])
     low_relfreq_perc = len([tri for tri in sent_dict_entry['trigrams'] if fdist['fd'][' '.join(tri)] < 4]) / sent_length
     perfect_prob_perc = len([prob for prob in sent_dict_entry['prob_list'] if prob == 1]) / sent_length
-    
-    if verbose:
-        print(
-            f"Sentence: \"{sent_dict_entry['text']}\"\n"
-            f"Sentence length (in trigrams): {sent_length}\n"
-            f"Percentage of low trigram relfreq (count<4): {round(low_relfreq_perc*100)}%\n"
-            f"Percentage of perfect probability: {round(perfect_prob_perc*100)}%\n"
-            f"Shannon entropy: {round(shan,4)}\n"
-            f"Shannon entropy (length normalised): {round(shan_lengthnorm,4)}\n"
-            f"Shannon-McMillan-Breiman entropy (~ length normalised): {round(shan_mcmill_brei,4)}\n"
-            f"Shannon entropy (relative frequency weighted): {round(shan_relfreqweight,4)}\n"
-            f"Shannon entropy (length normalised + relfreq weighted): {round(shan_length_relfreq,4)}\n"
-        )
     
     sent_dict_entry["sent_length"] = sent_length
     sent_dict_entry["low_relfreq_perc"] = low_relfreq_perc
@@ -153,25 +152,39 @@ def entropy_measures(sent_dict_entry, fdist, lm, verbose=False):
     
     return sent_dict_entry
 
-def get_random_sample_ent(sent_dict, fdist, lm):
-    sent_amount = len(sent_dict)
+def get_random_sample_ent(df):
+    """Get the entropy measures for a random sentence from the dataset."""
+    sent_amount = df.shape[0]
     rand_id = random.randint(0,sent_amount+1)
-    sent_dict_entry = sent_dict[rand_id]
-    entropy_measures(sent_dict_entry, fdist, lm, verbose=True) # ADJUST TO DF
+    
+    print(
+        f"Sentence: \"{df.iloc[[rand_id]]['text'].values[0]}\"\n"
+        f"Sentence length (in trigrams): {df.iloc[[rand_id]]['sent_length'].values[0]}\n"
+        f"Percentage of low trigram relfreq (count<4): {round(df.iloc[[rand_id]]['low_relfreq_perc'].values[0]*100)}%\n"
+        f"Percentage of perfect probability: {round(df.iloc[[rand_id]]['perfect_prob_perc'].values[0]*100)}%\n"
+        f"Shannon entropy: {round(df.iloc[[rand_id]]['shan'].values[0],4)}\n"
+        f"Shannon entropy (length normalised): {round(df.iloc[[rand_id]]['shan_lengthnorm'].values[0],4)}\n"
+        f"Shannon-McMillan-Breiman entropy (~ length normalised): {round(df.iloc[[rand_id]]['shan_mcmill_brei'].values[0],4)}\n"
+        f"Shannon entropy (relative frequency weighted): {round(df.iloc[[rand_id]]['shan_relfreqweight'].values[0],4)}\n"
+        f"Shannon entropy (length normalised + relfreq weighted): {round(df.iloc[[rand_id]]['shan_length_relfreq'].values[0],4)}\n"
+    )
+
 
 def get_corr_pairplot(df):
+    """Make a pairplot of scatterplots, including correlation scores."""
     def corrfunc(x, y, hue=None, ax=None, **kws):
         """Plot the correlation coefficient in the top left hand corner of a plot."""
         r, _ = stats.pearsonr(x, y)
         ax = ax or plt.gca()
         ax.annotate(f"ρ = {r:.2f}", xy=(.7, .9), xycoords=ax.transAxes)
 
-    p = sns.pairplot(df, color='#002757')
+    p = sns.pairplot(df)
     p.map_lower(corrfunc)
 
     return p
 
 def get_freq_counts(df, lm, fdist, index):
+    """For a sentence, retrieve the trigram and unigram counts."""
     tri_list = []
     uni_list = []
     
